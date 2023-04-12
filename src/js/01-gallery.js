c45858ea4bc4291370cd41735ec8e94190a686fd
TatianaPrkhmnk <tatiashka11@gmail.com>
2023-04-12 18:14:23 +0300

    video

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
-// Add imports above this line
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const photos = galleryItems.map(({ preview, original, description }) =>
+  `<li><a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a> </li>`
+).join('');
+
+gallery.insertAdjacentHTML('beforeend', photos);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
+});
